db628c297cbca34adf4e111a541b8cfc625ac519
kocjanr <kocjanr@onid.oregonstate.edu>
2018-06-08 15:13:19 -0700

    Update sketch_jun04a.ino

--- sketch_jun04a/sketch_jun04a.ino
+++ sketch_jun04a/sketch_jun04a.ino
@@ -6,19 +6,49 @@ void setup() {
   FS fileSystem;
   FCB x;
 
-  
-  fileSystem.initalize();
+  //reformat and initalize
   fileSystem.reformat();
+  fileSystem.initalize();
   
-  fileSystem.create("file.txt");
-  fileSystem.create("hellow.txt");
-  fileSystem.create("junk.txt");
-  fileSystem.create("file.txt");
+  //create some files
+  fileSystem.create("top_secret_stuff.txt");
+  fileSystem.create("bottom_secret_stuff.txt");
+  fileSystem.create("do_not_open.txt");
+  fileSystem.create("sweet_car_pics.txt");
+  Serial.println();
+
+  //list files
   fileSystem.list();
+  Serial.println();
+
+  //open a file
+  fileSystem.openFile("top_secret_stuff.txt",&x);
+  Serial << x.fileName << " loaded in from eprom" << endl;
+  Serial.println();
+
+  //wite to file "top_secret_stuff.txt"
+  fileSystem.writeFile(&x,"mic check number 1",18);
+  Serial.println();
+  fileSystem.fileSeek(&x);
+  Serial.println();
   
-  fileSystem.openFile("file.txt",&x);
-  Serial << "x file name:: ";
-  Serial << x.fileName << endl;
+  fileSystem.writeFile(&x,"isnt this a lot of fun?",23);
+  Serial.println();
+  fileSystem.fileSeek(&x);
+  Serial.println();
+
+  
+  //close a file
+  //fileSystem.closeFile(&x);
+  //Serial.println();
+  //fileSystem.list();
+
+  //delete a file
+  //fileSystem.deleteFile("top_secret_stuff.txt");
+  //fileSystem.list();
+
+   //fileSystem.reformat();
+  
 }
 
 void loop() {
